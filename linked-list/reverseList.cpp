df6f893dbace14e4c2cdd2e9da0c99b78fb4b330
xubo <1457800511@qq.com>
2026-09-26 10:33:37 +0800

    feat: 反转链表

---
+++ linked-list/reverseList.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <map>
+#include <iostream>
+#include <algorithm>
+using namespace std; 
+
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *cur = head, *prev = nullptr;
+
+        while (cur != nullptr) {
+            ListNode *nxt = cur->next;
+
+            cur->next = prev;
+            prev = cur;
+
+            cur = nxt;
+        }
+
+        return prev;
+    }
+};
